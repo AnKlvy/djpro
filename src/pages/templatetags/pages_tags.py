80b34bae0d0e107ed917382bf453c09ebb11e48c
AnKlvy <andreyklimov.ver2@gmail.com>
2023-03-05 21:50:25 +0600

    add inclusion tag

--- src/pages/templatetags/pages_tags.py
+++ src/pages/templatetags/pages_tags.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.simple_tag()
 def get_categories():
     return ProdCategories.objects.all()
+
+@register.inclusion_tag('pages/list_categories.html')
+def show_categories():
+    cats = ProdCategories.objects.all()
+    return {"cats":cats}
